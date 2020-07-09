2319fb21d5816216064f5f322a887555aefc36ba
Gagan Singh <gagansingh.dev@outlook.com>
2020-07-09 12:51:16 -0400

    Implemented useState, useEffect & useCallback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,32 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
-
+import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
 const Ingredients = () => {
    const [userIngredients, setUserIngredients] = useState([]);
+   const [isLoading, setIsLoading] = useState(false);
+   const [error, setError] = useState();
+
+   useEffect(() => console.log("rendering"), [userIngredients]);
+
+   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
+      setUserIngredients(filteredIngredients);
+   }, []);
 
    const addIngredientHandler = (ingredient) => {
-      setUserIngredients((prevIngredients) => [
-         ...prevIngredients,
-         { id: Math.random().toString(), ...ingredient },
-      ]);
+      setIsLoading(true);
+
+      fetch(`${process.env.REACT_APP_BACKEND_URL}/ingredients.json`, {
+         method: "POST",
+         body: JSON.stringify(ingredient), // Converts [] or {} in JSON
+         headers: { "Content-Type": "application/json" },
+      })
+         .then((response) => {
+            setIsLoading(false);
+            return response.json();
+         })
+         .then((responseData) => {
+            setUserIngredients((prevIngredients) => [
+               ...prevIngredients,
+               { id: responseData.name, ...ingredient },
+            ]);
+         })
+         .catch((err) => {
+            setError(err.message);
+         });
    };
 
    const removeIngredientHandler = (id) => {
-      fetch(`${process.env.REACT_APP_BACKEND_URL}/ingredients`);
+      setIsLoading(true);
 
-      setUserIngredients(userIngredients.filter((ing) => ing.id !== id));
+      fetch(`${process.env.REACT_APP_BACKEND_URL}/ingredients/${id}.json`, {
+         method: "DELETE",
+      })
+         .then((response) => {
+            setIsLoading(false);
+            setUserIngredients(userIngredients.filter((ing) => ing.id !== id));
+         })
+         .catch((err) => {
+            setError("Oops! Something is not working here.");
+            setIsLoading(false);
+         });
+   };
+
+   const clearError = () => {
+      setError(null);
    };
 
    return (
       <div className="App">
-         <IngredientForm onAddIngredient={addIngredientHandler} />
+         {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+         <IngredientForm
+            onAddIngredient={addIngredientHandler}
+            loading={isLoading}
+         />
 
          <section>
-            <Search />
+            <Search onLoadIngredients={filteredIngredientsHandler} />
             <IngredientList
                ingredients={userIngredients}
                onRemoveItem={(id) => removeIngredientHandler(id)}
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" />
-        </div>
-      </Card>
-    </section>
-  );
+const Search = React.memo((props) => {
+   const { onLoadIngredients } = props;
+   const [enteredFilter, setEnteredFilter] = useState("");
+   const inputRef = useRef();
+
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         if (enteredFilter === inputRef.current.value) {
+            const query =
+               enteredFilter.length === 0
+                  ? ""
+                  : `?orderBy="title"&equalTo="${enteredFilter}"`;
+
+            fetch(
+               `${process.env.REACT_APP_BACKEND_URL}/ingredients.json${query}`
+            )
+               .then((response) => response.json())
+               .then((responseData) => {
+                  const loadedIngredients = [];
+                  for (const key in responseData) {
+                     loadedIngredients.push({
+                        id: key,
+                        title: responseData[key].title,
+                        amount: responseData[key].amount,
+                     });
+                  }
+                  onLoadIngredients(loadedIngredients);
+               });
+         }
+      }, 500);
+
+      // cleanup for setTimeout
+      return () => {
+         clearTimeout(timer);
+      };
+   }, [enteredFilter, onLoadIngredients, inputRef]);
+
+   return (
+      <section className="search">
+         <Card>
+            <div className="search-input">
+               <label>Filter by Title</label>
+               <input
+                  ref={inputRef}
+                  type="text"
+                  value={enteredFilter}
+                  onChange={(event) => setEnteredFilter(event.target.value)}
+               />
+            </div>
+         </Card>
+      </section>
+   );
 });
 
 export default Search;
